e310c85171d4be3161f29e5afc9c8394a90e719c
Isnu Nasrudin <isnunas@gmail.com>
2026-03-04 01:08:52 +0700

    message auth failed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title></title>
+    <title>Login - {{ config('app.name') }}</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <link rel="icon" href="{{ asset('img/icon.ico') }}" type="image/x-icon" />
 
@@ -18,9 +18,7 @@
                 "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands",
                     "simple-line-icons"
                 ],
-                urls: ['{{ asset('
-                    css / fonts.min.css ') }}'
-                ]
+                urls: ['{{ asset('css/fonts.min.css') }}']
             },
             active: function() {
                 sessionStorage.fonts = true;
@@ -59,6 +57,15 @@
                     <hr class="flex-grow-1">
                 </div>
                 <form class="login-form" method="POST">
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     @csrf
                     <div class="form-group">
                         <label for="email" class="placeholder"><b>Email</b></label>
@@ -82,55 +89,6 @@
                             class="btn btn-secondary col-md-5 float-right mt-3 mt-sm-0 fw-bold">Masuk</button>
                     </div>
                 </form>
-            </div>
-
-            <div class="container container-signup container-transparent animated fadeIn">
-                <h3 class="text-center">Sign Up</h3>
-                <div class="login-form">
-                    <div class="form-group">
-                        <label for="fullname" class="placeholder"><b>Fullname</b></label>
-                        <input id="fullname" name="fullname" type="text" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email" class="placeholder"><b>Email</b></label>
-                        <input id="email" name="email" type="email" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="passwordsignin" class="placeholder"><b>Password</b></label>
-                        <div class="position-relative">
-                            <input id="passwordsignin" name="passwordsignin" type="password" class="form-control"
-                                required>
-                            <div class="show-password">
-                                <i class="icon-eye"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="confirmpassword" class="placeholder"><b>Confirm Password</b></label>
-                        <div class="position-relative">
-                            <input id="confirmpassword" name="confirmpassword" type="password" class="form-control"
-                                required>
-                            <div class="show-password">
-                                <i class="icon-eye"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row form-sub m-0">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" name="agree" id="agree">
-                            <label class="custom-control-label" for="agree">I Agree the terms and
-                                conditions.</label>
-                        </div>
-                    </div>
-                    <div class="row form-action">
-                        <div class="col-md-6">
-                            <a href="#" id="show-signin" class="btn btn-danger btn-link w-100 fw-bold">Cancel</a>
-                        </div>
-                        <div class="col-md-6">
-                            <a href="#" class="btn btn-secondary w-100 fw-bold">Sign Up</a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
